d194cd4d58efcf5742ef50c3f1c40e18631917e2
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-07-27 00:19:25 +0100

    new

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,15 +1,13 @@
 pub fn arrange_phrase(phrase: &str) -> String {
-    let mut res: Vec<String> = vec![];
+    let mut res: Vec<String> = vec![]; 
     let table: Vec<&str> = phrase.split_whitespace().collect();
-    let mut i = 0;
-    while i < table.len() {
-        let digit_str = &(res.len() + 1).to_string();
-        if table[i].contains(digit_str) {
-            res.push(table[i].replace(digit_str, ""));
-            i = 0;
-            continue;
+    while res.len() < table.len() {
+        let digit_str = &(res.len() + 1).to_string(); 
+        for i in 0..table.len() {
+            if table[i].contains(digit_str) {
+                res.push(table[i].replace(digit_str , ""));
+            }
         }
-        i += 1;
     }
     res.join(" ")
 }
