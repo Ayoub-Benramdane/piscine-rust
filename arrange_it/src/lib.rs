35363ffc35ae1cef57b48615039b7a38db7f2a61
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-07-26 19:45:05 +0100

    new

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -3,7 +3,7 @@ pub fn arrange_phrase(phrase: &str) -> String {
     let mut result = String::new();
 
     for i in 1..=9 {
-        let digit = (b'0' + i as u8) as char;  // مثلاً: 1 -> '1'
+        let digit = (b'0' + i as u8) as char;
         if let Some(word) = words.clone().find(|w| w.contains(digit)) {
             for ch in word.chars() {
                 if ch != digit {
