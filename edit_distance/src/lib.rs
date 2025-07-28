7ae34b4642981de475e2a81ca7c6bbb979a5bb36
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-07-28 23:30:07 +0100

    new

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -4,37 +4,62 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
     let mut source_chars: Vec<char> = source.chars().collect();
     let target_chars: Vec<char> = target.chars().collect();
 
-    let len_source = source_chars.len();
+    let mut len_source = source_chars.len();
     let len_target = target_chars.len();
     let mut del: usize;
     let mut source_or_target = "source";
+    let mut min: usize;
     let mut count = 0;
     if len_source > len_target {
+        min = len_target;
         del = len_source - len_target;
     } else {
-        del = len_target -len_source;
-        source_or_target = "target"
+        min = len_source;
+        del = len_target - len_source;
+        source_or_target = "target";
     }
 
-    for i in 0..len_source {
+    let mut i = 0;
+    while i < len_source {
         for j in 0..len_target {
             if i == j {
                 if source_chars[i] != target_chars[j] {
-                    if del > 0  && source_or_target == "source" {
+                    let mut next = false;
+                    if i + 1 < min {
+                        if source_chars[i + 1] == target_chars[j + 1] {
+                            next = true;
+                        }
+                    }
+                    if !next && del > 0 && source_or_target == "source" {
                         del -= 1;
                         source_chars.remove(i);
-                    } else if del > 0 && source_or_target == "target" {
+                        len_source -= 1;
+                    } else if !next && del > 0 && source_or_target == "target" {
+                        min += 1;
                         del -= 1;
                         source_chars.insert(i, target_chars[j]);
+                        len_source += 1;
                     } else {
-                        let _ = mem::replace(&mut source_chars[i], target_chars[j]);
+                        let _ = std::mem::replace(&mut source_chars[i], target_chars[j]);
                     }
                     count += 1;
                 }
             } else if j > i {
-                break
+                break;
             }
         }
+
+        if del != 0 && i == len_source - 1 {
+            del -= 1;
+            source_chars.insert(len_source, '1');
+            len_source += 1;
+            i = 0;
+            continue;
+        }
+        i += 1;
     }
+    let source_str: String = source_chars.iter().collect();
+    let target_str: String = target_chars.iter().collect();
+    println!("{}, {}", source_str, target_str);
     count
 }
--- edit_distance/src/main.rs
+++ edit_distance/src/main.rs
@@ -5,9 +5,27 @@ fn main() {
     let target = "assignment";
 
     println!(
-        "It's necessary to make {} change(s) to {:?} to get {:?}",
+        "1    It's necessary to make {} change(s) to {:?} to get {:?}",
         edit_distance(source, target),
         source,
         target
+    );
+    println!(
+        "2    It's necessary to make {} change(s) to {:?} to get {:?}",
+        edit_distance("gumbo", "gambol"),
+        "gumbo",
+        "gambol"
+    );
+    println!(
+        "3    It's necessary to make {} change(s) to {:?} to get {:?}",
+        edit_distance("kitten", "sitting"),
+        "kitten",
+        "sitting"
+    );
+    println!(
+        "4    It's necessary to make {} change(s) to {:?} to get {:?}",
+        edit_distance("rosettacode", "raisethysword"),
+        "rosettacode",
+        "raisethysword"
     );
 }
