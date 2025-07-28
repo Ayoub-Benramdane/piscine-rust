0c956fb52ad3d6d2954594ef9671bdbf25336e03
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-07-28 23:44:22 +0100

    new

---
+++ bigger/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let mut max = i32::MIN;
+    for (_, value) in h {
+        if value > max {
+            max = value
+        }
+    }
+    max
+}
