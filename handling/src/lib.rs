ae37eb16e4187a8b004c96a7b4f9fb8fce3b2f80
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-07-31 22:26:04 +0100

    new

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -6,8 +6,8 @@ pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     let mut file = OpenOptions::new()
         .read(true)
         .write(true)
+        .append(true)
         .create(true)
-        .truncate(true)
         .open(path)
         .unwrap();
     write!(file, "{}", content).unwrap()
