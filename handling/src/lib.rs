440f9cd3caef50333ca21e2b21758d9590176e9b
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-07-31 22:20:37 +0100

    new

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -2,13 +2,13 @@ use std::io::Write;
 use std::path::Path;
 use std::fs::OpenOptions;
 
-
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    
-let mut file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(path).unwrap();
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path)
+        .unwrap();
     write!(file, "{}", content).unwrap()
 }
