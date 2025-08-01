3348008ba98f291a8f3ba9373cb8a7e33258da48
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-08-01 22:35:54 +0100

    new

---
+++ error_types/src/lib.rs
@@ -0,0 +1,70 @@
+use chrono::Local;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError <'a> {
+   pub form_values: (&'a str, String),
+   pub date: String,
+   pub err: &'a str,
+}
+
+impl FormError <'static> {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        let now = Local::now();
+        Self {
+            form_values: (field_name, field_value),
+            date: now.format("%Y-%m-%d %H:%M:%S").to_string(),
+            err: err,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new("name", self.name.clone(), "Username is empty"));
+        }
+        if self.password.len() < 8 {
+            return Err(
+                FormError::new(
+                    "name",
+                    self.name.clone(),
+                    "Password should be at least 8 characters long"
+                )
+            );
+        } else {
+            let valid = {
+                let mut alpha = false;
+                let mut digit = false;
+                let mut sym = false;
+
+                for c in self.password.chars() {
+                    if c.is_ascii_alphabetic() {
+                        alpha = true;
+                    } else if c.is_ascii_digit() {
+                        digit = true;
+                    } else {
+                        sym = true;
+                    }
+                }
+                alpha && digit && sym
+            };
+
+            if !valid {
+                return Err(
+                    FormError::new(
+                        "name",
+                        self.name.clone(),
+                        "Password should be a combination of ASCII numbers, letters and symbols"
+                    )
+                );
+            };
+        }
+        Ok(())
+    }
+}
