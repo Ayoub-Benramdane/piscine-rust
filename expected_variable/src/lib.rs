52685a35bf340f289c2693898573c689463401b6
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-08-03 00:52:11 +0100

    new

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,9 +1,13 @@
 use edit_distance::*;
+use convert_case::{Casing};
 
 pub fn expected_variable(source: &str, target: &str) -> Option<String> {
     let source_to_lower = source.to_lowercase();
     let target_to_lower = target.to_lowercase();
     let dist = edit_distance(&source_to_lower, &target_to_lower);
+    if target_to_lower != target_to_lower.to_case(convert_case::Case::Camel) && target_to_lower != target_to_lower.to_case(convert_case::Case::Snake) {
+        return None
+    }
     let prc = 100.0 - (dist as f64 / target.len() as f64 *100.0);
     if prc > 50.0 {
         Some(format!("{}%", prc.round()))
