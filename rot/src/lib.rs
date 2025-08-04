97aa3e6673874a316a1c2dd85f5a6a106066f29b
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-08-04 21:27:58 +0100

    new

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -4,14 +4,13 @@ pub fn rotate(input: &str, mut key: i8) -> String {
         key = 26 - key.abs();
     }
 
-    for c in input.chars() {
+    for mut c in input.chars() {
         if c.is_alphabetic() {
             let base = if c.is_ascii_uppercase() { 'A' } else { 'a' };
             let offset_base = ((c as u8) - (base as u8) + (key as u8)) % 26;
-            res.push((offset_base + (base as u8)) as char);
-        } else {
-            res.push(c);
+            c = (offset_base + (base as u8)) as char
         }
+        res.push(c);
     }
     res
 }
