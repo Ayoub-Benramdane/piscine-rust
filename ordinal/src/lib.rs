760a5ee6d46d9c543b8c6cc9dca8b6599288c6c8
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-08-04 18:52:24 +0100

    new

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn num_to_ordinal(x: u32) -> String {
+    let mut nb: Vec<char> = x.to_string().chars().collect();
+    let last = nb.pop().unwrap();
+    match last  {
+        '1' if x % 100 != 11 => {
+            let mut res = x.to_string();
+            res.push_str("st");
+            return res
+        }
+        '2' if x % 100 != 12 => {
+            let mut res = x.to_string();
+            res.push_str("nd");
+            return res
+        }
+        '3' if x % 100 != 13 => {
+            let mut res = x.to_string();
+            res.push_str("rd");
+            return res
+        }
+        _ =>{}.
+    };
+    let mut res = x.to_string();
+    res.push_str("th");
+    res
+}
