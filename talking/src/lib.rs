950c6fe9908b0db4dad4de0808a1d039bcd0b107
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-08-04 20:44:19 +0100

    new

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -5,9 +5,10 @@ pub fn talking(text: &str) -> &str {
     }
 
     let qst = trim_text.ends_with("?");
+    let chars = trim_text.chars().any(|c| c.is_alphabetic());
     let upper = text.chars().filter(|c| c.is_alphabetic()).all(|c| c.is_uppercase());
-
-    match (upper, qst) {
+    let yelling = upper && chars;
+    match (yelling, qst) {
         (true, true) => "Quiet, I am thinking!",
         (true, false) => "There is no need to yell, calm down!",
         (false, true) => "Sure.",
