2ac7cd4c07111301128776f0e20b328c13cffecf
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-08-11 18:58:22 +0100

    new

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -2,47 +2,59 @@ use std::fmt::Display;
 
 #[derive(Debug)]
 pub struct Player {
-	pub name: String,
-	pub strength: f64,
-	pub score: i32,
-	pub money: i32,
-	pub weapons: Vec<String>,
+    pub name: String,
+    pub strength: f64,
+    pub score: i32,
+    pub money: i32,
+    pub weapons: Vec<String>,
 }
 
 pub struct Fruit {
-	pub weight_in_kg: f64,
+    pub weight_in_kg: f64,
 }
 
 pub struct Meat {
-	pub weight_in_kg: f64,
-	pub fat_content: f64,
+    pub weight_in_kg: f64,
+    pub fat_content: f64,
 }
 
 impl Player {
-	pub fn eat<T: Food>(&mut self, food: T) {
-		self.strength += food.gives();
-	}
+    pub fn eat<T: Food>(&mut self, food: T) {
+        self.strength += food.gives();
+    }
 }
 
 pub trait Food {
-	fn gives(&self) -> f64;
+    fn gives(&self) -> f64;
 }
 
-impl Food for Fruit { 
+impl Food for Fruit {
     fn gives(&self) -> f64 {
-        self.weight_in_kg*4.
+        self.weight_in_kg * 4.0
     }
 }
 
 impl Food for Meat {
     fn gives(&self) -> f64 {
-        let meat = self.weight_in_kg - self.fat_content;
-        meat*4.+self.fat_content*5.+4.0
+		let meat = self.weight_in_kg - self.fat_content;
+        if self.fat_content == 0.0 {
+            meat * 4.0
+        } else {
+            meat * 4.0 + self.fat_content * 5.0 + 4.0
+        }
     }
 }
 
 impl Display for Player {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}\nStrength: {}, Score: {}, Money: {}\nWeapons: {:?}", self.name,self.strength , self.score, self.money, self.weapons)
-	}
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}\nStrength: {}, Score: {}, Money: {}\nWeapons: {:?}",
+            self.name,
+            self.strength,
+            self.score,
+            self.money,
+            self.weapons
+        )
+    }
 }
