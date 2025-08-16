3d1da6311b18aa7dfbc3e7860bd452b5d3d6f098
Ayoub BENRAMDANE <ayoubbenramdane05@gmail.com>
2025-08-16 23:14:34 +0100

    new

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -10,48 +10,41 @@ impl Store {
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Cart {
-    pub items : Vec<(String, f32)>,
-    pub receipt : Vec<f32>,
+    pub items: Vec<(String, f32)>,
+    pub receipt: Vec<f32>,
 }
 impl Cart {
     pub fn new() -> Cart {
-        Cart{items : vec![], receipt : vec![]}
+        Cart { items: vec![], receipt: vec![] }
     }
     pub fn insert_item(&mut self, s: &Store, ele: String) {
-        let mut  price = 0.0 ;
-        for (i,v) in &s.products {
+        let mut price = 0.0;
+        for (i, v) in &s.products {
             if *i == ele {
-                price = *v ;
+                price = *v;
             }
         }
         self.items.push((ele, price))
     }
     pub fn generate_receipt(&mut self) -> Vec<f32> {
         let mut a = vec![];
-        for (_,v) in &self.items {
-            a.push(*v) ;
+        for (_, v) in &self.items {
+            a.push(*v);
         }
         a.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        let mut i  = 0;
         let mut b = vec![];
-        let mut j = 0.;
         for aa in &a {
-            j = *aa - ((*aa)*a[0]/a.iter().sum::<f32>());
-            b.push(format(j)) ;
-
-            i+= 1;
-
+            let j = *aa - (*aa * a[0]) / a.iter().sum::<f32>();
+            b.push(format(j));
         }
-        
-        self.receipt = b.clone() ;
+
+        self.receipt = b.clone();
         b
     }
-
 }
 
-pub fn format(a : f32)->f32 {
-    let mut ab = a.to_string();
-    ab = format!("{:.02}", a);
+pub fn format(a: f32) -> f32 {
+    let ab = format!("{:.02}", a);
     let c = ab.parse::<f32>().unwrap_or(0.0);
     c
 }
